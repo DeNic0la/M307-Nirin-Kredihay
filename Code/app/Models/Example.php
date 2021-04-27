382adde7712c35ec666a6e32e7614d4df5944887
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2021-04-27 14:38:44 +0200

    Upgraded Framework and added gitignore

--- Code/app/Models/Example.php
+++ Code/app/Models/Example.php
@@ -1,26 +1,63 @@
 <?php
 class Example
 {
-	public int $id;
-	public string $name;
+    private static string $tableName = 'Example';//Need to know wich table to Connect to
+    /**
+     * @var array|string[]
+     */
+    protected static array $readOnly = ['id'];// This Values cannot be written
+    protected PDO $pdo;
+    protected $_data = array(
+        'id' => '',
+        'name' => '',
+    );
 
-	public function __construct(int $id, string $name)
-	{
-		$this->id = $id;
-		$this->name = $name;
-	}
 
-	public function getById(int $id): ?self
-	{
-		$statement = db()->prepare('SELECT * FROM examples WHERE id = :id LIMIT 1');
-		$statement->bindParam(':id', $id);
-		$statement->execute();
 
-		$result = $statement->fetch();
-		if ( ! $result) {
-			return null;
-		}
+    public function __get($name) {
+        if (isset($this->_data[$name])) {
+            return $this->_data[$name];
+        } else {
+            return null;
+        }
+    }
+    /**
+     * @throws Exception
+     */
+    public function __set($name, $value) {
+        if (in_array($name,$this->readOnly)) {
+            throw new Exception("not allowed : $name");
+        } else {
+            $this->_data[$name] = $value;
+        }
+    }
 
-		return new self($result['id'], $result['name']);
-	}
+
+    public function __construct(string $name)
+    {
+        $this->pdo = db();
+        $this->name = $name;
+    }
+
+    public static function getById(int $id): ?self
+    {
+        $statement = db()->prepare('SELECT * FROM '.Example::$tableName.' WHERE id = :id LIMIT 1');
+        $statement->bindParam(':id', $id);
+        $statement->execute();
+
+        $result = $statement->fetch();
+        if ( ! $result) {
+            return null;
+        }
+
+        return new self($result['name']);
+    }
+
+    public function save(){
+        $Connection = db();
+        $statement = $Connection->prepare('INSERT INTO '.Example::$tableName.' (name) VALUES (:name)');
+        $statement->bindParam(':name',$this->name);
+        $statement->execute();
+        $this->_data['id'] = $Connection->lastInsertId();
+    }
 }
--- Code/core/helpers.php
+++ Code/core/helpers.php
@@ -8,6 +8,12 @@ function e(string $value): string
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', false);
 }
 
+function abort(int $code){
+    http_response_code($code);
+    //Include 404 //(404 Page Displaying)
+    die();
+}
+
 /**
  * Nutze diese Funktion um auf einen POST-Wert
  * zuzugreifen.
@@ -22,7 +28,6 @@ function post(string $key, $default = '')
  * Datenbankverbindung als PDO zurück.
  */
 $dbInstance = null;
-
 function db(): PDO
 {
     global $dbInstance;
@@ -30,13 +35,78 @@ function db(): PDO
     if ($dbInstance) {
         return $dbInstance;
     }
+    global $DataBaseConfig;
 
     try {
-        $dbInstance = new PDO('mysql:host=127.0.0.1;dbname=' . $db['name'], $db['username'], $db['password'], [
+        $dbInstance = new PDO('mysql:host=127.0.0.1;dbname=' . $DataBaseConfig['name'], $DataBaseConfig['username'], $DataBaseConfig['password'], [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
         ]);
+        return $dbInstance;
     } catch (PDOException $e) {
         die('Keine Verbindung zur Datenbank möglich: ' . $e->getMessage());
     }
+}
+
+function validateRequest(array $Superglobal,array $fields):array{
+    $Validated = [];
+    $Errors = [];
+    $FormularFields = [];
+    $HasErrors = false;
+    foreach ($fields as $field=>$rules){
+        $validationRules = explode("|",$rules);
+        $value = $Superglobal[$field] ?? '';
+        $FormularFields[$field] = $value;
+        foreach ($validationRules as $rule){
+            $ruleArray = explode(":",$rule);
+            switch($ruleArray[0]){
+                case "required":
+                    if ($value === ''){
+                        $HasErrors = true;
+                        $Errors[$field] = 'Dieses Feld ist Erforderlich';
+                        continue 3;
+                    }
+                    break;
+                case "integer":
+                    if ($value !== '' && !ctype_digit($value)){
+                        $HasErrors = true;
+                        $Errors[$field] = 'Bitte Geben sie eine Gültige Zahl ein';
+                        continue 3;
+                    }
+                    elseif ($value !== ''){
+                        $value = intval($value);
+                    }
+                    break;
+                case "email":
+                    if ($value !== '' && !preg_match("/[^@]+@[^.]+\..+$/i" ,$value)){
+                        $HasErrors = true;
+                        $Errors[$field] = 'Geben sie eine Gültige E-Mail adresse ein';
+                        continue 3;
+                    }
+                    break;
+                case "regex":
+                    if ($value !== '' && !preg_match($ruleArray[1],$value)){
+                        $HasErrors = true;
+                        $Errors[$field] = 'Dieses Feld ist Ungültig';
+                        continue 3;
+                    }
+                    break;
+                case "phone":
+                    if ($value !== '' && !preg_match("/(\b(0041|0)|\B\+41)(\s?\(0\))?(\s)?[1-9]{2}(\s)?[0-9]{3}(\s)?[0-9]{2}(\s)?[0-9]{2}\b/",$value)){
+                        $HasErrors = true;
+                        $Errors[$field] = 'Keine Gültige Telefon nummer';
+                        continue 3;
+                    }
+                    break;
+
+            }
+        }
+        $Validated[$field] = $value;
+    }
+    return array(
+        'hasErrors' => $HasErrors,
+        'Validated' => $Validated,
+        'Errors' => $Errors,
+        'Fields' => $FormularFields,
+    );
 }

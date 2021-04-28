56e1bbeb45521af16c51f972b72e6071f70120e0
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2021-04-28 16:23:59 +0200

    Add Card to Create

--- Code/app/Views/create.view.php
+++ Code/app/Views/create.view.php
@@ -28,49 +28,58 @@
             <div class="content">
                 <ul id="errorDisplayer"></ul>
                 <div class="container-fluid">
-                    <form action="/add" method="POST" id="createForm">
-                        <div class="form-group">
-                            <label for="prename">Vorname</label>
-                            <input value="<?= $OldValues['prename'] ?? ''?>" type="text" class="form-control" id="prename" name="prename" placeholder="Max">
-                            <div class="text-danger" id="error-prename"><?= $Errors['prename'] ?? ''?></div>
+                    <div class="card">
+                        <div class="card-header card-chart card-header-warning card-header-width">
+                            <div class="ct-chart" id="dailySalesChart">
+                                <a class="fa fa-pencil fa-lg" aria-hidden="true"></a>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="lastname">Name</label>
-                            <input value="<?= $OldValues['lastname'] ?? ''?>" type="text" class="form-control" id="lastname" name="lastname" placeholder="Muster">
-                            <div class="text-danger" id="error-lastname"><?= $Errors['lastname'] ?? ''?></div>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">E-Mail</label>
-                            <input value="<?= $OldValues['email'] ?? ''?>" type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
-                            <div class="text-danger" id="error-email"><?= $Errors['email'] ?? ''?></div>
-                        </div>
-                        <div class="form-group">
-                            <label for="tel">Telefonnummer</label>
-                            <input value="<?= $OldValues['tel'] ?? ''?>" type="text" class="form-control" id="tel" name="tel" placeholder="+41 xx xxx xx xx">
-                            <div class="text-danger" id="error-tel"><?= $Errors['tel'] ?? ''?></div>
-                        </div>
-                        <div class="form-group">
-                            <label for="rate">Anzahl Raten</label>
-                            <input value="<?= $OldValues['rate'] ?? ''?>" type="Number" class="form-control" id="rate" name="rate" placeholder="Number of Rates">
-                            <div class="text-danger" id="error-rate"><?= $Errors['rate'] ?? ''?></div>
-                        </div>
-                        <div class="form-group">
-                            <label for="package">Packet</label>
-                            <select class="form-control selectpicker" data-style="btn btn-link" id="package" name="package">
-                                <option hidden>Wählen sie Aus</option>
-                                <?php
-                                    foreach ($CreditPackages as $CreditPackage){
+                        <div class="card-body list-block">
+                            <form action="/add" method="POST" id="createForm">
+                                <div class="form-group">
+                                    <label for="prename">Vorname</label>
+                                    <input value="<?= $OldValues['prename'] ?? ''?>" type="text" class="form-control" id="prename" name="prename" placeholder="Max">
+                                    <div class="text-danger" id="error-prename"><?= $Errors['prename'] ?? ''?></div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="lastname">Name</label>
+                                    <input value="<?= $OldValues['lastname'] ?? ''?>" type="text" class="form-control" id="lastname" name="lastname" placeholder="Muster">
+                                    <div class="text-danger" id="error-lastname"><?= $Errors['lastname'] ?? ''?></div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">E-Mail</label>
+                                    <input value="<?= $OldValues['email'] ?? ''?>" type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                                    <div class="text-danger" id="error-email"><?= $Errors['email'] ?? ''?></div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="tel">Telefonnummer</label>
+                                    <input value="<?= $OldValues['tel'] ?? ''?>" type="text" class="form-control" id="tel" name="tel" placeholder="+41 xx xxx xx xx">
+                                    <div class="text-danger" id="error-tel"><?= $Errors['tel'] ?? ''?></div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="rate">Anzahl Raten</label>
+                                    <input value="<?= $OldValues['rate'] ?? ''?>" type="Number" class="form-control" id="rate" name="rate" placeholder="Number of Rates">
+                                    <div class="text-danger" id="error-rate"><?= $Errors['rate'] ?? ''?></div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="package">Packet</label>
+                                    <select class="form-control selectpicker" data-style="btn btn-link" id="package" name="package">
+                                        <option hidden>Wählen sie Aus</option>
+                                        <?php
+                                        foreach ($CreditPackages as $CreditPackage){
+                                            ?>
+                                            <option <?= isset($OldValues['package'])&&$OldValues['package']===$CreditPackage->id ? 'selected':'' ?> value="<?= $CreditPackage->id; ?>"><?= $CreditPackage->name; ?></option>
+                                            <?php
+                                        }
                                         ?>
-                                <option <?= isset($OldValues['package'])&&$OldValues['package']===$CreditPackage->id ? 'selected':'' ?> value="<?= $CreditPackage->id; ?>"><?= $CreditPackage->name; ?></option>
-                                <?php
-                                    }
-                                ?>
-                            </select>
-                            <div class="text-danger" id="error-package"><?= $Errors['package'] ?? ''?></div>
+                                    </select>
+                                    <div class="text-danger" id="error-package"><?= $Errors['package'] ?? ''?></div>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Erstellen</button>
+                                <a href="./" class="btn btn-danger">Abbrechen</a>
+                            </form>
                         </div>
-                        <button type="submit" class="btn btn-primary">Erstellen</button>
-                        <a href="./" class="btn btn-danger">Abbrechen</a>
-                    </form>
+                    </div>
                     <script src="./public/js/create.view.js"></script>
                 </div>
             </div>

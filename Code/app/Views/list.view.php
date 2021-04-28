58b06e358ed5ef70d10dea2d199015b88ccf4d3d
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2021-04-28 14:45:24 +0200

    show list as table

--- Code/app/Views/list.view.php
+++ Code/app/Views/list.view.php
@@ -31,6 +31,7 @@
                         </div>
                     </div>
                     <div class="card-body list-block">
+                        <!--
                         <ul class="list">
                             <?php foreach($loans as $loan) : ?>
                             <li class="item">
@@ -43,6 +44,51 @@
                             </li>
                             <?php endforeach; ?>
                         </ul>
+                         -->
+
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>E-Mail</th>
+                                <th>Telefon</th>
+                                <th>Raten</th>
+                                <th>Packet</th>
+                                <th class="text-center">Status</th>
+                                <th class="text-right">Aktion</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach($loans as $loan) : ?>
+                            <tr>
+                                <td>
+                                    <div class="form-check form-check-inline">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                                            <span class="form-check-sign">
+                                            <span class="check"></span>
+                                        </span>
+                                        </label>
+                                    </div>
+                                </td>
+                                <td><?= $loan->prename ," ", $loan->lastname?></td>
+                                <td><?= $loan->email ?></td>
+                                <td><?= $loan->phone ?></td>
+                                <td><?= $loan->rate, " Raten" ?></td>
+                                <td><?= $loan->getPackage()->name ?? "" ?></td>
+                                <td class="text-center">
+                                    <i class="material-icons">bolt</i>
+                                </td>
+                                <td class="td-actions text-right">
+                                    <button type="button" rel="tooltip" class="btn btn-primary">
+                                        <i class="material-icons">edit</i>
+                                    </button>
+                                </td>
+                            </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>

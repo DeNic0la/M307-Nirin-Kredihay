f04264aa120a7b7fa5dcd34860fda477bb44ba3a
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2021-04-28 09:09:42 +0200

    404-Site style

--- Code/app/Views/404.view.php
+++ Code/app/Views/404.view.php
@@ -5,9 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./public/css/404.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Seite nicht gefunden</title>
 </head>
 <body>
-<h1>404</h1>
+    <div class="message_container">
+        <h1>404</h1>
+        <h2>Diese Seite kann nicht gefunden werden</h2>
+        <a class="btn btn-primary btn-lg" href="./home" role="button">
+            Back to Home</a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </body>
 </html>
